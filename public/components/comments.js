c2109ceb672ea512a5372a78bb7fd022fc696162
javathunderman <javathunderman@emposoft.tk>
2016-02-20 17:54:26 +0000

    formatting update

--- public/components/comments.js
+++ public/components/comments.js
@@ -1,48 +1,47 @@
 var CommentBox = React.createClass({
-    getInitialState: function () {
+    getInitialState: function() {
         return {
             comments: null
         };
     },
-    componentDidMount: function () {
+    componentDidMount: function() {
         var that = this;
         this.socket = io();
-        this.socket.on('comments', function (comments) {
-            that.setState({ comments: comments });
+        this.socket.on('comments', function(comments) {
+            that.setState({
+                comments: comments
+            });
         });
         this.socket.emit('fetchComments');
     },
-    submitComment: function (comment, callback) {
-        this.socket.emit('newComment', comment, function (err) {
-            if (err)
-                return console.error('New comment error:', err);
+    submitComment: function(comment, callback) {
+        this.socket.emit('newComment', comment, function(err) {
+            if(err) return console.error('New comment error:', err);
             callback();
         });
     },
     render: function() {
-        return (
-            <div className="commentBox">
-                <h1>Zoover Activity</h1>
-                <br />
-                <h2>Here's what's going on right now.</h2>
-                <CommentList comments={this.state.comments}/>
-                <CommentForm submitComment={this.submitComment}/>
-            </div>
-        );
+        return( < div className = "commentBox" > < h1 > Zoover Activity < /h1>
+                <br / > < h2 > Here 's what'
+            s going on right now. < /h2>
+                <CommentList comments={this.state.comments}/ > < CommentForm submitComment = {
+                this.submitComment
+            }
+            />
+            </div > );
     }
 });
 var CommentList = React.createClass({
-    render: function () {
-        var Comments = (<div>Loading Zoover...</div>);
+        render: function() {
+            var Comments = ( < div > Loading Zoover... < /div>);
         if (this.props.comments) {
             Comments = this.props.comments.map(function (comment) {
-                return (<Comment comment={comment} />);
-            });
-        }
-        return (
-            <div className="commentList">
-				{Comments}
-            </div>
+                return (<Comment comment={comment} / > );
+        });
+}
+return( < div className = "commentList" > {
+Comments
+} < /div>
         );
     }
 });
@@ -50,41 +49,46 @@ var Comment = React.createClass({
     render: function () {
         return (
             <div className="comment">
-                <span className="author">{this.props.comment.author}</span> said:<br/>
-                <div className="body">{this.props.comment.text}</div>
-            </div>
-        );
-    }
+                <span className="author">{this.props.comment.author}</span > said: < br / > < div className = "body" > {
+this.props.comment.text
+} < /div>
+            </div > );
+}
 });
 var CommentForm = React.createClass({
-    handleSubmit: function (e) {
+    handleSubmit: function(e) {
         e.preventDefault();
         var that = this;
         var author = this.refs.author.getDOMNode().value.trim();
         var text = this.refs.text.getDOMNode().value.trim();
-        var comment = { author: author, text: text };
+        var comment = {
+            author: author,
+            text: text
+        };
         var submitButton = this.refs.submitButton.getDOMNode();
         submitButton.innerHTML = 'Zooving...';
         submitButton.setAttribute('disabled', 'disabled');
-        this.props.submitComment(comment, function (err) {
+        this.props.submitComment(comment, function(err) {
             that.refs.author.getDOMNode().value = '';
             that.refs.text.getDOMNode().value = '';
             submitButton.innerHTML = 'Zoov!';
             submitButton.removeAttribute('disabled');
         });
     },
-    render: function () {
-        return (
-            <form className="commentForm" onSubmit={this.handleSubmit}>
-                <input type="text" name="author" ref="author" placeholder="Name" required /><br/>
-                <textarea name="text" ref="text" placeholder="Zoover" required></textarea><br/>
-                <button type="submit" ref="submitButton">Zoov!</button>
-            </form>
-        );
+    render: function() {
+        return( < form className = "commentForm"
+            onSubmit = {
+                this.handleSubmit
+            } > < input type = "text"
+            name = "author"
+            ref = "author"
+            placeholder = "Name"
+            required / > < br / > < textarea name = "text"
+            ref = "text"
+            placeholder = "Zoover"
+            required > < /textarea><br/ > < button type = "submit"
+            ref = "submitButton" > Zoov! < /button>
+            </form > );
     }
 });
-
-React.render(
-    <CommentBox/>,
-    document.getElementById('content')
-);
+React.render( < CommentBox / > , document.getElementById('content'));
--- setup-passport.js
+++ setup-passport.js
@@ -1,27 +1,22 @@
 var passport = require('passport');
 var Auth0Strategy = require('passport-auth0');
-
 var strategy = new Auth0Strategy({
-    domain:       'emposoft.auth0.com',
-    clientID:     'e6RroHpjwIETB0wgw9DrW7r8wN05139S',
+    domain: 'emposoft.auth0.com',
+    clientID: 'e6RroHpjwIETB0wgw9DrW7r8wN05139S',
     clientSecret: 'lolyulookhere',
-    callbackURL:  '/callback'
-  }, function(accessToken, refreshToken, extraParams, profile, done) {
+    callbackURL: '/callback'
+}, function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
     // profile has all the information from the user
     return done(null, profile);
-  });
-
+});
 passport.use(strategy);
-
 // This is not a best practice, but we want to keep things simple for now
 passport.serializeUser(function(user, done) {
-  done(null, user);
+    done(null, user);
 });
-
 passport.deserializeUser(function(user, done) {
-  done(null, user);
+    done(null, user);
 });
-
 module.exports = strategy;
